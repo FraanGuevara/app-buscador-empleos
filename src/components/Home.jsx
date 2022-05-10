b30ee6cb8a20ff1584e60197de4a00affefcb882
FraanGuevara <fraan.guevara1@gmail.com>
2022-05-09 22:02:48 -0300

    creacion componente home

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../App.css';
+
+export default function Home() {
+  return (
+    <>
+        <div className='home'>
+            <div>
+                IMG DE BIENVENIDA MAS BOTON
+            </div>
+            <div>
+                Nombre de la empresa y leyenda + botones login/sign up
+            </div>
+        </div>
+    </>
+  )
+}
