7e15b2a8e47813a83715c6d0ce8ff9076d27e629
ignacioborraz <ignacioborraz@hotmail.com>
2022-05-14 12:58:01 -0300

    form

--- src/pages/Ingresar.jsx
+++ src/pages/Ingresar.jsx
@@ -1,114 +1,222 @@
-import React, {useState} from 'react'
+import React, {useState,useContext,useRef} from 'react'
+import {authContext} from '../context/AuthContext'
+import {Link as LinkRouter} from "react-router-dom"
 
 import Box from '@mui/material/Box'
 import MailIcon from '@mui/icons-material/Mail'
 import KeyIcon from '@mui/icons-material/Key'
 import Typography from '@mui/material/Typography'
-import GoogleIcon from '@mui/icons-material/Google'
-
-import {Link as LinkRouter} from "react-router-dom"
 
 import '../App.css'
 
 export default function Ingresar(props) {
 
     const [register,setRegister] = useState(false)
-    const [mail,setMail] = useState("")
-    const [pass,setPass] = useState("")
+    const context = useContext(authContext)
+    const mail = useRef()
+    const pass = useRef()
+    const name = useRef()
+
+    const mainApi = "http://localhost:4000/api/"
     
     const handleRegister = (event) => {
         setRegister(!register)
     }
 
-/*     const handleSubmit = (event) => { //funcion necesaria para pasar los datos al back, chequear cuando tengamos la API
-        event.preventDefault() //prevenimos la accion del submit
-        const userLogin = {
-            email: mail,
-			password: pass,
-			from: "LogInForm"
-		}
-        props.logInUser(userLogin)
+    const handleSignin = (event) => {
+        event.preventDefault()
+        fetch(mainApi+"auth/login", {
+            method:"POST",
+            headers:{"Content-Type": "application/json"},
+            body:JSON.stringify({
+                email: mail.current.value,
+                password: pass.current.value
+            })
+        })
+        .then(res => res.json())
+        .then(data => {
+            localStorage.setItem("token",data.token)
+            context.setAuth({
+                id:data.user.id,
+                name:data.user.name,
+                logged:true
+            })
+            fetch(mainApi+"users", {
+                headers:{"Authorization":"Bearer "+localStorage.getItem("token")}
+            })
+            .then(response => response.json())
+            .then(data => console.log(data))
+        })
+        .catch(error => console.log(error))
     }
- */
+
+    const handleSignup = (event) => {
+        event.preventDefault()
+        fetch(mainApi+"auth/signup",{
+            method:"POST",
+            headers:{"Content-Type": "application/json"},
+            body:JSON.stringify({
+                name:name.current.value,
+                email: mail.current.value,
+                password:pass.current.value
+            })
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    }
+
     return (
         <div className='home'>
             <div className='home-img' />
             <div className='home-ingreso'>
                 <Typography variant="h1" sx={{fontFamily: 'Macondo', padding: '10px', color: 'white'}}>Bienvenido!</Typography>
-                {/* <form onSubmit={handleSubmit}> */}
-                <form className='w100'>
-                    <Box sx={{
-                        display: 'flex',
-                        width: '60%',
-                        minWidth: '280px',
-                        backgroundColor: 'rgb(0, 105, 192)',
-                        border: '5px solid rgb(0, 105, 192)',
-                        borderRadius: '50px'}}>
-                        <label htmlFor="email">
-                            <MailIcon sx={{
-                                display: 'flex',
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                                width: '40px',
-                                height: '40px',
-                                color: 'white',
-                                backgroundColor: 'rgb(0, 105, 192)',
-                                border: '0 solid rgb(0, 105, 192)',
-                                borderRadius: '50px 0 0 50px',
-                                padding: '10px'}}/>
-                        </label>
-                        <input type='email' name='email' id='email' placeholder='e-mail' className='myInput' value={mail} onChange={e=>setMail(e.target.value)} required />
-                    </Box>
-                    <Box sx={{
-                        display: 'flex',
-                        width: '60%',
-                        minWidth: '280px',
-                        backgroundColor: 'rgb(0, 105, 192)',
-                        border: '5px solid rgb(0, 105, 192)',
-                        borderRadius: '50px',
-                        marginTop: '5px'}}>
-                        <label htmlFor="password">
-                            <KeyIcon sx={{
-                                display: 'flex',
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                                width: '40px',
-                                height: '40px',
-                                color: 'white',
-                                backgroundColor: 'rgb(0, 105, 192)',
-                                border: '0 solid rgb(0, 105, 192)',
-                                borderRadius: '50px 0 0 50px',
-                                padding: '10px'}}/>
-                        </label>
-                        <input type='password' name='password' id='password' placeholder='contraseña' className='myInput' value={pass} onChange={e=>setPass(e.target.value)} required />
-                    </Box>
-                    <Box sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'space-between',
-                        width: '60%',
-                        minWidth: '280px',
-                        height: '50px',
-                        borderRadius: '50px',
-                        marginTop: '20px'}}>
-                        {/* <GoogleSignIn /> */}
-                        <input type="submit" value='ingreso' className='myButton' required />
-                        <GoogleIcon sx={{
-                                display: 'flex',
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                                width: '50px',
-                                height: '50px',
-                                color: 'white',
-                                backgroundColor: 'rgb(0, 105, 192)',
-                                '&:hover': {bgcolor: 'rgb(19, 127, 217)'},
-                                border: '0px solid rgb(0, 105, 192)',
-                                borderRadius: '50px',
-                                padding: '10px'}}/>
-                        <div className='myButtonDiv'>registro</div>
-                    </Box>
-                </form>
+                {register ? (
+                    <form onSubmit={handleSignup} className='w100'>
+                        <Box sx={{
+                            display: 'flex',
+                            width: '60%',
+                            minWidth: '280px',
+                            backgroundColor: 'rgb(0, 105, 192)',
+                            border: '5px solid rgb(0, 105, 192)',
+                            borderRadius: '50px'}}>
+                            <label htmlFor="name">
+                                <MailIcon sx={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    width: '40px',
+                                    height: '40px',
+                                    color: 'white',
+                                    backgroundColor: 'rgb(0, 105, 192)',
+                                    border: '0 solid rgb(0, 105, 192)',
+                                    borderRadius: '50px 0 0 50px',
+                                    padding: '10px'}}/>
+                            </label>
+                            <input type='text' name='name' id='name' placeholder='nombre' className='myInput' ref={name} required />
+                        </Box>
+                        <Box sx={{
+                            display: 'flex',
+                            width: '60%',
+                            minWidth: '280px',
+                            backgroundColor: 'rgb(0, 105, 192)',
+                            border: '5px solid rgb(0, 105, 192)',
+                            borderRadius: '50px'}}>
+                            <label htmlFor="email">
+                                <MailIcon sx={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    width: '40px',
+                                    height: '40px',
+                                    color: 'white',
+                                    backgroundColor: 'rgb(0, 105, 192)',
+                                    border: '0 solid rgb(0, 105, 192)',
+                                    borderRadius: '50px 0 0 50px',
+                                    padding: '10px'}}/>
+                            </label>
+                            <input type='email' name='email' id='email' placeholder='e-mail' className='myInput' ref={mail} required />
+                        </Box>
+                        <Box sx={{
+                            display: 'flex',
+                            width: '60%',
+                            minWidth: '280px',
+                            backgroundColor: 'rgb(0, 105, 192)',
+                            border: '5px solid rgb(0, 105, 192)',
+                            borderRadius: '50px',
+                            marginTop: '5px'}}>
+                            <label htmlFor="password">
+                                <KeyIcon sx={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    width: '40px',
+                                    height: '40px',
+                                    color: 'white',
+                                    backgroundColor: 'rgb(0, 105, 192)',
+                                    border: '0 solid rgb(0, 105, 192)',
+                                    borderRadius: '50px 0 0 50px',
+                                    padding: '10px'}}/>
+                            </label>
+                            <input type='password' name='password' id='password' placeholder='contraseña' className='myInput' ref={pass} required />
+                        </Box>
+                        <Box sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'space-between',
+                            width: '60%',
+                            minWidth: '280px',
+                            height: '50px',
+                            borderRadius: '50px',
+                            marginTop: '20px'}}>
+                            <input type="submit" value='ingreso' className='myButton' required />
+                            <div className='myButtonDiv'>registro</div>
+                        </Box>
+                    </form>
+
+                ) : (
+                    <form onSubmit={handleSignin} className='w100'>
+                        <Box sx={{
+                            display: 'flex',
+                            width: '60%',
+                            minWidth: '280px',
+                            backgroundColor: 'rgb(0, 105, 192)',
+                            border: '5px solid rgb(0, 105, 192)',
+                            borderRadius: '50px'}}>
+                            <label htmlFor="email">
+                                <MailIcon sx={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    width: '40px',
+                                    height: '40px',
+                                    color: 'white',
+                                    backgroundColor: 'rgb(0, 105, 192)',
+                                    border: '0 solid rgb(0, 105, 192)',
+                                    borderRadius: '50px 0 0 50px',
+                                    padding: '10px'}}/>
+                            </label>
+                            <input type='email' name='email' id='email' placeholder='e-mail' className='myInput' value={mail} ref={mail} required />
+                        </Box>
+                        <Box sx={{
+                            display: 'flex',
+                            width: '60%',
+                            minWidth: '280px',
+                            backgroundColor: 'rgb(0, 105, 192)',
+                            border: '5px solid rgb(0, 105, 192)',
+                            borderRadius: '50px',
+                            marginTop: '5px'}}>
+                            <label htmlFor="password">
+                                <KeyIcon sx={{
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    width: '40px',
+                                    height: '40px',
+                                    color: 'white',
+                                    backgroundColor: 'rgb(0, 105, 192)',
+                                    border: '0 solid rgb(0, 105, 192)',
+                                    borderRadius: '50px 0 0 50px',
+                                    padding: '10px'}}/>
+                            </label>
+                            <input type='password' name='password' id='password' placeholder='contraseña' className='myInput' value={pass} ref={pass} required />
+                        </Box>
+                        <Box sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'space-between',
+                            width: '60%',
+                            minWidth: '280px',
+                            height: '50px',
+                            borderRadius: '50px',
+                            marginTop: '20px'}}>
+                            <input type="submit" value='ingreso' className='myButton' required />
+                            <div className='myButtonDiv'>registro</div>
+                        </Box>
+                    </form>
+                )}
             </div>
         </div>
     )
+
 }
